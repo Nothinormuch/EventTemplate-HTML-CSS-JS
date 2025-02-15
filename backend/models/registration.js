37c098a2f90aa5498c15316d356db25f3c6ac0ca
Nikhil Sharma <Nikhil.12509@stu.upes.ac.in>
2025-02-15 23:34:20 +0530

    Update registration.js

--- backend/models/registration.js
+++ backend/models/registration.js
@@ -54,158 +54,26 @@ const registrationSchema = new mongoose.Schema({
         required: false,
         default: "-",
     },
-    memberTwoName: {
-        type: String,
-        required: true,
-    },
-    memberTwoEmail: {
-        type: String,
-        required: true,
-    },
-    memberTwoPhone: {
-        type: String,
-        required: true,
-    },
-    memberTwoCourse: {
-        type: String,
-        required: true,
-    },
-    memberTwoYearOfStudy: {
-        type: String,
-        required: true,
-    },
-    memberTwoUPESStudent: {
-        type: String,
-        required: true,
-        default: "Yes",
-    },
-    memberTwoCollegeName: {
-        type: String,
-        required: false,
-        default: "UPES",
-    },
-    memberTwoSapID: {
-        type: String,
-        required: true,
-    },
-    memberTwoCSAMember: {
-        type: String,
-        required: true,
-        default: "No",
-    },
-    memberTwoCSAID: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreeName: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreeEmail: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreePhone: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreeCourse: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreeYearOfStudy: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreeUPESStudent: {
-        type: String,
-        required: false,
-        default: "Yes",
-    },
-    memberThreeCollegeName: {
-        type: String,
-        required: false,
-        default: "UPES",
-    },
-    memberThreeSapID: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberThreeCSAMember: {
-        type: String,
-        required: false,
-        default: "No",
-    },
-    memberThreeCSAID: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourName: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourEmail: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourPhone: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourCourse: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourYearOfStudy: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourUPESStudent: {
-        type: String,
-        required: false,
-        default: "Yes",
-    },
-    memberFourCollegeName: {
-        type: String,
-        required: false,
-        default: "UPES",
-    },
-    memberFourSapID: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    memberFourCSAMember: {
-        type: String,
-        required: false,
-        default: "No",
-    },
-    memberFourCSAID: {
-        type: String,
-        required: false,
-        default: "-",
-    },
-    transactionID: {
-        type: String,
-        required: true,
-    },
-    transactionSS: {
-        type: String,
-        required: true,
-    },
+    members: [{
+        name: {
+            type: String,
+            required: true,
+        },
+        year: {
+            type: String,
+            required: true,
+        },
+        sapID: {
+            type: String,
+            required: false,
+            default: "-",
+        },
+        csaID: {
+            type: String,
+            required: false,
+            default: "-",
+        }
+    }],
     createdAt: {
         type: Date,
         default: () => moment().tz("Asia/Kolkata").toDate(),
